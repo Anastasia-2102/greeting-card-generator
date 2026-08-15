7dae51d3e898d20277ad3a6f1b2e66ed3bdda4b6
Anastasia <ketchersid18@mail.ru>
2026-08-15 17:21:10 -0500

    the generator lives on the page, closes #2

---
+++ card.js
@@ -0,0 +1,17 @@
+let nameInput = document.getElementById("name-input");
+let adjectiveInput = document.getElementById("adjective-input");
+let nounInput = document.getElementById("noun-input");
+let makeButton = document.getElementById("make-button");
+let cardText = document.getElementById("card-text");
+
+function makeCard() {
+  let friendName = nameInput.value;
+  let adjective = adjectiveInput.value;
+  let noun = nounInput.value;
+
+  cardText.textContent =
+    "Dear " + friendName + ", you are as " + adjective +
+    " as a " + noun + ". Happy birthday from the generator.";
+}
+
+makeButton.addEventListener("click", makeCard);
